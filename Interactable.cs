77daac7ee9968171e035999998de94cb41472eac
marcoslagram <marcoslagram@hotmail.com>
2018-10-22 21:46:42 +0200

    Interactable primera versión

---
+++ Interactable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+    public float maxdistance = 3f;
+    public float distancia;
+    public GameObject gal;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, maxdistance);
+    }
+
+}
