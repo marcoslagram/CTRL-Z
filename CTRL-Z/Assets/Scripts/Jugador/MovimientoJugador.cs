2c9dc465f02d15ebee3d2a9dcfcd6e00a2b0ea8d
marcoslagram <marcoslagram@hotmail.com>
2018-10-31 12:24:36 +0100

    Corregido Transport

--- CTRL-Z/Assets/Scripts/Jugador/MovimientoJugador.cs
+++ CTRL-Z/Assets/Scripts/Jugador/MovimientoJugador.cs
@@ -6,14 +6,15 @@ public class MovimientoJugador : MonoBehaviour {
 
     [SerializeField] private float velocidad = 2f;
     [SerializeField] private float velocidadCarrera = 10f;
-
+    Rigidbody myPlayer;
 
     private Transform myTransform;
 
     // Use this for initialization
     void Start () {
-        
-        myTransform = GetComponent<Transform>();
+
+        myPlayer = GetComponent<Rigidbody>();
+       // myTransform = GetComponent<Transform>();
     }
 	
 	// Update is called once per frame
@@ -26,24 +27,30 @@ public class MovimientoJugador : MonoBehaviour {
 
     private void DesplazamientoNormal()
     {
+
+      // Vector3 movement = new Vector3(0, 0, velocidad) * Time.deltaTime;
         if (Input.GetKey(KeyCode.W))  //cuando pulsas W va hacia adelante
         {
-            myTransform.Translate(new Vector3(0, 0, velocidad) * Time.deltaTime);
+            myPlayer.MovePosition(transform.position + (new Vector3(0, 0, velocidad) * Time.deltaTime));
+          //  myTransform.Translate(new Vector3(0, 0, velocidad) * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.S))  //cuando pulsas S va hacia atrás
         {
-            myTransform.Translate(new Vector3(0, 0, -velocidad) * Time.deltaTime);
+          //  myTransform.Translate(new Vector3(0, 0, -velocidad) * Time.deltaTime);
+            myPlayer.MovePosition(transform.position + (new Vector3(0, 0, -velocidad) * Time.deltaTime));
         }
 
         if (Input.GetKey(KeyCode.A))  //cuando pulsas A va hacia la izquierda
         {
-            myTransform.Translate(new Vector3(-velocidad, 0, 0) * Time.deltaTime);
+            myPlayer.MovePosition(transform.position + (new Vector3(-velocidad, 0, 0) * Time.deltaTime));
+          //  myTransform.Translate(new Vector3(-velocidad, 0, 0) * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.D))  //cuando pulsas D va hacia la derecha
         {
-            myTransform.Translate(new Vector3(+velocidad, 0, 0) * Time.deltaTime);
+            myPlayer.MovePosition(transform.position + (new Vector3(+velocidad, 0, 0) * Time.deltaTime));
+          //  myTransform.Translate(new Vector3(+velocidad, 0, 0) * Time.deltaTime);
         }
     }
 
@@ -51,22 +58,26 @@ public class MovimientoJugador : MonoBehaviour {
     {
         if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))  //cuando pulsas W va hacia adelante
         {
-            myTransform.Translate(new Vector3(0, 0, velocidadCarrera) * Time.deltaTime);
+            myPlayer.MovePosition(transform.position + (new Vector3(0, 0, velocidadCarrera) * Time.deltaTime));
+         //   myTransform.Translate(new Vector3(0, 0, velocidadCarrera) * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.S))  //cuando pulsas S va hacia atrás
         {
-            myTransform.Translate(new Vector3(0, 0, -velocidadCarrera) * Time.deltaTime);
+            myPlayer.MovePosition(transform.position + (new Vector3(0, 0, -velocidadCarrera) * Time.deltaTime));
+           // myTransform.Translate(new Vector3(0, 0, -velocidadCarrera) * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.LeftShift)&& Input.GetKey(KeyCode.A))  //cuando pulsas A va hacia la izquierda
         {
-            myTransform.Translate(new Vector3(velocidadCarrera, 0, 0) * Time.deltaTime);
+            myPlayer.MovePosition(transform.position + (new Vector3(velocidadCarrera, 0, 0) * Time.deltaTime));
+           // myTransform.Translate(new Vector3(velocidadCarrera, 0, 0) * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.LeftShift)&& Input.GetKey(KeyCode.D))  //cuando pulsas D va hacia la derecha
         {
-            myTransform.Translate(new Vector3(-velocidadCarrera, 0, 0) * Time.deltaTime);
+            myPlayer.MovePosition(transform.position + (new Vector3(-velocidadCarrera, 0, 0) * Time.deltaTime));
+           // myTransform.Translate(new Vector3(-velocidadCarrera, 0, 0) * Time.deltaTime);
         }
     }
 
